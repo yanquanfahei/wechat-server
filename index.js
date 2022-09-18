be31c2642bbd22fa24b7d46ff2c579da8f0be50d
yanquanfahei <1449826851@qq.com>
2022-09-18 21:59:48 +0800

    feat: push test

--- index.js
+++ index.js
@@ -49,6 +49,24 @@ app.get("/api/wx_openid", async (req, res) => {
   }
 });
 
+// 小程序消息推送
+app.all('/push', async (req, res) => {
+  const headers = req.headers
+  const token = headers['x-wx-cloudbase-access-token']
+  const weixinAPI = `https://api.weixin.qq.com/cgi-bin/message/custom/send?cloudbase_access_token=${token}`
+  const payload = {
+      touser: headers['x-wx-openid'],
+      msgtype: 'text',
+      text: {
+          content: `云托管接收消息推送成功，内容如下：\n${JSON.stringify(req.body, null, 2)}`
+      }
+  }
+  // dispatch to wx server
+  const result = await client.post(weixinAPI, payload)
+  console.log('received request', req.body, result.data)
+  res.send('success')
+});
+
 const port = process.env.PORT || 80;
 
 async function bootstrap() {
